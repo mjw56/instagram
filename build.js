d0ce0abcf96b8e7154e0612a7f607c1ddac07ec6
Mike Wilcox <mwilcox56@gmail.com>
2018-02-10 15:37:55 -0500

    add sass build support

--- build.js
+++ build.js
@@ -1,15 +1,22 @@
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
+const scss = require('rollup-plugin-scss');
+const { writeFileSync } = require('fs');
 
 const plugins = [
+    scss({
+      output: function (styles, styleNodes) {
+        writeFileSync('build/bundle.css', styles)
+      },
+    }),
     babel({
         exclude: 'node_modules/**',
         plugins: ['inferno'],
-    }),
+    })
   ]
 
 rollup.rollup({
-    input: 'app/pages/timeline.jsx',
+    input: 'client/pages/timeline/timeline.jsx',
     plugins
   }).then(bundle => {
     bundle.write({
